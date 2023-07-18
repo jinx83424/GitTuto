e3d2398cb405467dcf4f4cc9777f1c4cbc283914
jinx83424 <ajinkya83424@gmail.com>
2023-07-18 22:33:44 +0530

    Added DOM.js file and update git.html code

---
+++ dom.js
@@ -0,0 +1,6 @@
+let header=document.getElementById("header-title");
+header.style.borderBottom="solid 2px black";
+let heading=document.getElementsByClassName("title");
+console.log(heading)
+heading[0].style.fontWeight="bold";
+heading[0].style.color="green";
