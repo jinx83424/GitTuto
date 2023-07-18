03680fb35b58c8bfc0c57bbe111644dc7c9ca7dc
jinx83424 <ajinkya83424@gmail.com>
2023-07-18 22:52:00 +0530

    changes in dom.js >>change bgcolor and list customization

--- dom.js
+++ dom.js
@@ -1,6 +1,17 @@
 let header=document.getElementById("header-title");
 header.style.borderBottom="solid 2px black";
 let heading=document.getElementsByClassName("title");
-console.log(heading)
+// console.log(heading)
 heading[0].style.fontWeight="bold";
 heading[0].style.color="green";
+
+
+//solution of GetElementsbyClassname
+var itms=document.getElementsByClassName('list-group-item')
+itms[2].style.backgroundColor='green';
+
+for (let i=0;i<itms.length;i++){
+    itms[i].style.fontWeight="bold";
+    itms[i].style.color='blue';
+}
+
