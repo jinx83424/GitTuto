9085657d7d507263286f30a569fd071fb9294a0b
jinx83424 <ajinkya83424@gmail.com>
2023-07-19 13:45:31 +0530

    added query selector code

--- dom.js
+++ dom.js
@@ -1,23 +1,29 @@
-let header=document.getElementById("header-title");
-header.style.borderBottom="solid 2px black";
-let heading=document.getElementsByClassName("title");
-// console.log(heading)
-heading[0].style.fontWeight="bold";
-heading[0].style.color="green";
+// let header=document.getElementById("header-title");
+// header.style.borderBottom="solid 2px black";
+// let heading=document.getElementsByClassName("title");
+// // console.log(heading)
+// heading[0].style.fontWeight="bold";
+// heading[0].style.color="green";
 
 
-//solution of GetElementsbyClassname
-var itms=document.getElementsByClassName('list-group-item')
-itms[2].style.backgroundColor='green';
+// //solution of GetElementsbyClassname
+// var itms=document.getElementsByClassName('list-group-item')
+// itms[2].style.backgroundColor='green';
 
-for (let i=0;i<itms.length;i++){
-    itms[i].style.fontWeight="bold";
-    itms[i].style.color='blue';
-}
-//solution of GetElementsbytagname
-var li=document.getElementsByTagName('li')
+// for (let i=0;i<itms.length;i++){
+//     itms[i].style.fontWeight="bold";
+//     itms[i].style.color='blue';
+// }
+// //solution of GetElementsbytagname
+// var li=document.getElementsByTagName('li')
 
-for (let i=0;i<li.length;i++){
-    li[i].style.backgroundColor='grey';
-}
+// for (let i=0;i<li.length;i++){
+//     li[i].style.backgroundColor='grey';
+// }
+
+let secItem =document.querySelector('.list-group-item:nth-child(2)');
+secItem.style.backgroundColor='green';
+
+let thirdItem =document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='none';
 
